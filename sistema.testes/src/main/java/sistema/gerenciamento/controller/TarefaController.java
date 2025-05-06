4729061a09456bda89a81394394084895068bfa7
MONICA JIULIANI LEAMARI <monica.leamari@solutis.net.br>
2025-05-06 10:57:51 -0300

    Desenvolvido @Get/@Put/@Delete

--- sistema.testes/src/main/java/sistema/gerenciamento/controller/TarefaController.java
+++ sistema.testes/src/main/java/sistema/gerenciamento/controller/TarefaController.java
@@ -7,6 +7,7 @@ import sistema.gerenciamento.modelo.Tarefa;
 import sistema.gerenciamento.servico.TarefaServico;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tarefa")
@@ -24,5 +25,27 @@ public class TarefaController {
     public ResponseEntity<List<Tarefa>> listar(){
         return ResponseEntity.status(200).body(servico.listar());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Tarefa>> buscarPorId(@PathVariable Integer id){
+        Optional<Tarefa> tarefa = servico.buscarId(id);
+        return ResponseEntity.status(200).body(tarefa);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Tarefa> atualizar(@PathVariable Integer id, @RequestBody Tarefa tarefa){
+        Tarefa tarefaAtualizada = servico.atualizarId(id,tarefa);
+        return ResponseEntity.status(200).body(tarefaAtualizada);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Tarefa> deletar(@PathVariable Integer id){
+        servico.deletarPorId(id);
+        return ResponseEntity.status(200).build();
+    }
+
+
+
 
 }
--- sistema.testes/src/main/java/sistema/gerenciamento/servico/TarefaServico.java
+++ sistema.testes/src/main/java/sistema/gerenciamento/servico/TarefaServico.java
@@ -1,11 +1,13 @@
 package sistema.gerenciamento.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import sistema.gerenciamento.modelo.Tarefa;
 import sistema.gerenciamento.repositorio.TarefaRepositorio;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TarefaServico {
@@ -20,4 +22,29 @@ public class TarefaServico {
     public List<Tarefa> listar() {
         return repositorio.findAll();
     }
+
+    public Optional<Tarefa> buscarId(Integer id){
+        Optional<Tarefa> tarefa = repositorio.findById(id);
+        return tarefa;
+    }
+
+    public Tarefa atualizarId(Integer id, Tarefa tarefa){
+
+           Tarefa tarefaEncontrada = repositorio.findById(id).orElseThrow(() -> new RuntimeException("Tarefa não encontrada"));
+
+            tarefaEncontrada.setId(id);
+            tarefaEncontrada.setDescricao(tarefa.getDescricao());
+            tarefaEncontrada.setTitulo(tarefa.getTitulo());
+            tarefaEncontrada.setStatus(tarefa.getStatus());
+
+            return repositorio.save(tarefaEncontrada);
+
+    }
+
+    public void deletarPorId(Integer id){
+        Tarefa tarefa = repositorio.findById(id).orElseThrow(() -> new RuntimeException("Tarefa não encontrada"));
+        repositorio.deleteById(id);
+    }
+
+
 }
